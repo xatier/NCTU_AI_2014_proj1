3713c96b3af947557bdedb1d4caedf774f81e088
xatier <xatierlike@gmail.com>
2014-03-07 01:24:02 +0800

    refactoring

--- neuralNet/neural.py
+++ neuralNet/neural.py
@@ -4,7 +4,6 @@ import math
 
 # input raws
 t = []
-label = [0, 1]
 
 w = [0.5] * 228
 
@@ -26,7 +25,7 @@ def run(t):
 
 
 
-def learning(t, label, w):
+def learning(t, w):
 
     k = i = 0
     learning_limit = 800
@@ -37,27 +36,28 @@ def learning(t, label, w):
 
         g = 1 / (1.0 + math.e**in_)
 
+        # label
         l = 0 if i < 400 else 1
 
-        # learing progress
-        if (label[l] == 0 and g <= 0.5) or \
-           (label[l] == 1 and g > 0.5):
+        # learning progress
+        if (l == 0 and g < 0.5) or \
+           (l == 1 and g > 0.5):
             pass
         else:
             # not correct
             k //= 2
-            Err = label[l] - g;
 
             # adjust weight
+            adj = 0.05 * (l - g) * g * (1 - g)
             for j in range(228):
-                w[j] += 0.05 * Err * g * (1-g) * t[i][j]
+                w[j] += adj * t[i][j]
 
         k += 1
         i = (i+1) % 800
 
 
 
-def test(t, label, w):
+def test(t, w):
     """ run training data """
 
     k = 0;
@@ -70,9 +70,9 @@ def test(t, label, w):
 
         l = 0 if i < 400 else 1
 
-        if label[l] == 0 and g <= 0.5:
+        if l == 0 and g < 0.5:
             k += 1
-        elif label[l] == 1 and g > 0.5:
+        elif l == 1 and g > 0.5:
             k += 1
 
 
@@ -80,5 +80,5 @@ def test(t, label, w):
 
 
 run(t)
-learning(t, label, w)
-test(t, label, w)
+learning(t, w)
+test(t, w)
