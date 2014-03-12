e7768e6f8b99ac2a848d46fd91dea8e69cbe7aa0
xatier <xatierlike@gmail.com>
2014-03-12 20:15:45 +0800

    add test part for homework

--- neuralNet/neural.py
+++ neuralNet/neural.py
@@ -32,7 +32,7 @@ def learning(t, w):
     # learning times
     while k < learning_limit:
 
-        in_ = -sum( t[i][j] * w[j] for j in range(228) )
+        in_ = -sum( [ t[i][j] * w[j] for j in range(228) ] )
 
         g = 1 / (1.0 + math.e**in_)
 
@@ -55,6 +55,16 @@ def learning(t, w):
         k += 1
         i = (i+1) % 800
 
+
+def check(l, w):
+    in_ = -sum( [ l[j] * w[j] for j in range(228) ] )
+
+    g = 1 / (1.0 + math.e**in_)
+
+    if g < 0.5:
+        return True
+    else:
+        return False
 
 
 def test(t, w):
@@ -82,3 +92,59 @@ def test(t, w):
 run(t)
 learning(t, w)
 test(t, w)
+
+
+# testing
+"""
+ct = 0
+c1t = 0   # c1 -> c1
+c1f = 0   # c1 -> c2
+c2t = 0   # c2 -> c2
+c2f = 0   # c2 -> c1
+
+# read the file again
+train_f = open("../TAdrop/train.txt", "r");
+
+for line in train_f:
+    l = [ int(x) for x in line.split() ]
+
+    if ct < 400:
+        if check(l, w):
+            c1t += 1
+        else:
+            c1f += 1
+    else:
+        if check(l, w):
+            c2f += 1
+        else:
+            c2t += 1
+
+    ct += 1
+
+train_f.close()
+
+print(c1t)
+print(c1f)
+print(c2f)
+print(c2t)
+"""
+
+"""
+387
+13
+0
+400
+"""
+
+# read the test file
+test_f = open("../TAdrop/test.txt", "r");
+
+for line in test_f:
+    l = [ int(x) for x in line.split() ]
+
+    if check(l, w):
+        print("1")
+    else:
+        print("2")
+
+test_f.close()
