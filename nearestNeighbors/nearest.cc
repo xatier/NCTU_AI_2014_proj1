bc8e4138766d042eed9b98fb419cce55692e730c
xatier <xatierlike@gmail.com>
2014-03-18 22:46:51 +0800

    move to C++

---
+++ nearestNeighbors/nearest.cc
@@ -0,0 +1,64 @@
+#include<cstdio>
+#include<cmath>
+
+int main (void) {
+
+    FILE* train = std::fopen("../TAdrop/train.txt", "r");
+    double tr[800][229];
+    int i, j, k;
+
+    for(i = 0; i < 800; ++i) {
+        tr[i][228] = 0;
+        for(j = 0; j < 228; ++j) {
+            std::fscanf(train, "%lf", &tr[i][j]);
+            tr[i][228] += tr[i][j]*tr[i][j];
+        }
+    }
+    std::fclose(train);
+
+    FILE* test = std::fopen("../TAdrop/test.txt", "r");
+    double te[200][229];
+
+    for(i = 0; i < 200; ++i) {
+        te[i][228] = 0;
+        for(j = 0; j < 228; ++j) {
+            std::fscanf(test, "%lf", &te[i][j]);
+            te[i][228] += te[i][j]*te[i][j];
+        }
+    }
+    std::fclose(test);
+
+    int one = 0;            //test
+    int two = 0;            //test
+
+    int r_i = 0;            //record index
+    double min_a = 0;       //minimum angle
+    FILE* output = std::fopen("./test_out", "w");
+    for(i = 0; i < 200; ++i){
+        r_i = 0;
+        min_a = 0;
+        for (j = 0; j < 800; ++j) {
+            double dot = 0;
+            double cosa = 0;
+            for (k = 0; k < 228; ++k) {
+                dot += te[i][k]*tr[j][k];
+            }
+            cosa = (dot*dot) / (te[i][228]*tr[j][228]);
+            if ((cosa-min_a) > 0) {
+                r_i = j;
+                min_a = cosa;
+            }
+        }
+
+        if (r_i < 400) {
+            std::fputs("1\n", output);
+            one++;
+        } else {
+            std::fputs("2\n", output);
+            two++;
+        }
+    }
+    std::fclose(output);
+    std::printf("1:%d, 2:%d\n", one ,two);
+    return 0;
+}
---
+++ nearestNeighbors/run.cc
@@ -0,0 +1,76 @@
+#include<cstdio>
+#include<cmath>
+
+int main (void) {
+
+    FILE* train = std::fopen("../TAdrop/train.txt", "r");
+    double tr[800][229];
+    int i, j, k;
+
+    for(i = 0; i < 800; ++i) {
+        tr[i][228] = 0;
+        for(j = 0; j < 228; ++j) {
+            std::fscanf(train, "%lf", &tr[i][j]);
+            tr[i][228] += tr[i][j]*tr[i][j];
+        }
+    }
+    std::fclose(train);
+
+    FILE* test = std::fopen("../TAdrop/train.txt", "r");
+    double te[800][229];
+
+    for(i = 0; i < 800; ++i) {
+        te[i][228] = 0;
+        for(j = 0; j < 228; ++j) {
+            std::fscanf(test, "%lf", &te[i][j]);
+            te[i][228] += te[i][j]*te[i][j];
+        }
+    }
+    std::fclose(test);
+
+    int one = 0;            //test
+    int two = 0;            //test
+    int one_ = 0;
+    int two_ = 0;
+
+    int r_i = 0;            //record index
+    double min_a = 0;       //minimum angle
+    FILE* output = stdout;
+    for(i = 0; i < 800; ++i) {
+        r_i = 0;
+        min_a = 0;
+        for (j = 0; j < 800; ++j) {
+            double dot = 0;
+            double cosa = 0;
+            for (k = 0; k < 228; ++k) {
+                dot += te[i][k]*tr[j][k];
+            }
+            cosa = (dot*dot) / (te[i][228]*tr[j][228]);
+            if ((cosa-min_a) > 0) {
+                r_i = j;
+                min_a = cosa;
+            }
+        }
+
+        if (r_i < 400) {
+            //std::fputs("1\n", output);
+            if (i < 400)
+                one++;
+            else
+                one_++;
+        } else {
+            //std::fputs("2\n", output);
+            if (i < 400)
+                two_++;
+            else
+                two++;
+
+        }
+    }
+    //std::fclose(output);
+    std::puts("= confusion matrix =");
+    std::printf("1:%d, 2:%d\n", one ,two);
+    std::printf("1x:%d, 2x:%d\n", one_ ,two_);
+    std::puts("====================");
+    return 0;
+}
